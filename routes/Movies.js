cfdd1213531db5a3c059abbba5bad8192e42cddc
emanuele23dev <emanuele.meggiarin@libero.it>
2024-12-10 18:16:47 +0100

    add show route

--- routes/Movies.js
+++ routes/Movies.js
@@ -5,10 +5,30 @@ const connection = require("../database/connection");
 router.get("/", (req, res) => {
   const sql = `SELECT * FROM movies`;
   connection.query(sql, (err, results) => {
-    console.log(results);
+    if (err) return res.status(500).json({ err: err });
 
     res.json({
       movies: results,
+      count: results.length,
+    });
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM movies WHERE id = ?`;
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ err: err });
+
+    if (!results.length)
+      return res.status(404).json({ message: "Movie not found" });
+
+    const movie = {
+      ...results[0],
+    };
+
+    res.json({
+      movie,
     });
   });
 });
